a536f3862fc2c5329b5881c72d361400dc038112
KatarzynaMartynaWojcik <katarzynamartyna.wojcik@gmail.com>
2023-12-07 01:22:47 +0100

    adding trip details view

---
+++ src/main/java/com/example/javatravel/controller/TripDetailsController.java
@@ -0,0 +1,27 @@
+package com.example.javatravel.controller;
+
+import com.example.javatravel.entity.TripEntity;
+import com.example.javatravel.service.TripService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class TripDetailsController {
+    private TripService tripService;
+
+    @Autowired
+    public TripDetailsController(TripService tripService) {
+        this.tripService = tripService;
+    }
+
+    @GetMapping("/trip_details/{id}")
+    public String getTripDetails (Model model,@PathVariable("id") Long tripId){
+        TripEntity trip = tripService.getTripById(tripId);
+        model.addAttribute("trip", trip);
+        return "tripDetails";
+    }
+}
